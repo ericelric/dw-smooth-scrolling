e935b516c38aa4d085d5fa3ec8ca7d731b7a2ced
ericelric <mewes.e@gmail.com>
2015-11-04 14:21:48 +0100

    first commit

---
+++ js/scrolling.js
@@ -0,0 +1,20 @@
+var $root = $('html, body'); //for increased performance, so that it doesn't run every single time an anchor is clicked
+var speed = 1000; //animation time in ms
+var animation = 'linear'; //animation style 'linear' works without jQuery UI
+
+//main function
+$('a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, speed, animation);
+    return false;
+});
+
+// helper functions just for the demonstration
+$(".user").click(function() {
+    animation = $(this).attr("data-name");
+});
+
+$("#ok").click(function() {
+    speed = parseInt($('#input-field').val());
+});
